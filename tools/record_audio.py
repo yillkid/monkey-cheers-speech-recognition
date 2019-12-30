3488ed8bdd2f9fbeee4dd8d4a93af66d7cc79ff2
yillkid <yillkid@gmail.com>
2019-12-30 14:52:46 +0000

    Add record tool

---
+++ tools/record_audio.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+print('Recording...')
+with mic as source:
+    audio = r.listen(source, timeout=3)
+
+with open("audio_file.wav", "wb") as file:
+    file.write(audio.get_wav_data())
